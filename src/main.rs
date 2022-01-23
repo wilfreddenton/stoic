0616f7b30664531c96ed778a03ba864dbeea5d59
wilfreddenton <dentonw3@gmail.com>
2022-01-22 22:11:49 -0500

    move templates to own file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub mod templates;
+
+use crate::templates::TemplateName;
 use chrono::prelude::*;
 use clap::{Parser, Subcommand};
 use handlebars::Handlebars;
@@ -8,7 +11,6 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use strum::IntoEnumIterator;
-use strum_macros::{Display, EnumIter};
 
 #[derive(Parser)]
 #[clap(version, about)]
@@ -73,94 +75,6 @@ const CSS_STR: &str = r#"
 const JS_STR: &str = r#"
 window.addEventListener('DOMContentLoaded', () => {});
 "#;
-
-const BASE_TEMPLATE: &str = r#"
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>{{title}}</title>
-    <link rel="stylesheet" href="/assets/style.css">
-    <script type="text/javascript" src="/assets/script.js" async defer></script>
-  </head>
-  <body>
-    <div id="container">
-      {{> (lookup this "page_type") }}
-      <footer>
-      </footer>
-    </div>
-  </body>
-</html>
-"#;
-
-const INDEX_TEMPLATE: &str = r#"
-<section>
- <h1>{{title}}</h1>
-</section
-"#;
-
-const PAGE_TEMPLATE: &str = r#"
-{{> nav}}
-<section>
-  {{{contents}}}
-</section>
-"#;
-
-const POSTS_TEMPLATE: &str = r#"
-{{> nav}}
-<section>
-  <ul id="posts-list">
-    {{#each posts}}
-    <li class="posts-list-item">
-      <div class="posts-list-item-title">{{this.title}}</div>
-      <div class="posts-list-item-time">{{this.created_at}}</div>
-    </li>
-    {{/each}}
-  </ul>
-</section>
-"#;
-
-const POST_TEMPLATE: &str = r#"
-{{> nav}}
-<section>
-  {{{contents}}}
-</section>
-"#;
-
-const NAV_TEMPLATE: &str = r#"
-<nav>
-  <a href="/">Home</a>
-  {{#each path}}
-    <span class="breadcrumb">></span>
-    <a href="{{link}}">{{name}}</a>
-  {{/each}}
-</nav>
-"#;
-
-#[derive(EnumIter, Display)]
-#[strum(serialize_all = "lowercase")]
-enum TemplateName {
-    Base,
-    Index,
-    Page,
-    Posts,
-    Post,
-    Nav,
-}
-
-impl TemplateName {
-    fn template_str(&self) -> &str {
-        match self {
-            TemplateName::Base => BASE_TEMPLATE,
-            TemplateName::Index => INDEX_TEMPLATE,
-            TemplateName::Page => PAGE_TEMPLATE,
-            TemplateName::Posts => POSTS_TEMPLATE,
-            TemplateName::Post => POST_TEMPLATE,
-            TemplateName::Nav => NAV_TEMPLATE,
-        }
-    }
-}
 
 #[derive(Serialize)]
 #[serde(rename_all = "lowercase")]
---
+++ src/templates.rs
@@ -0,0 +1,89 @@
+use strum_macros::{Display, EnumIter};
+
+const BASE_TEMPLATE: &str = r#"
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>{{title}}</title>
+    <link rel="stylesheet" href="/assets/style.css">
+    <script type="text/javascript" src="/assets/script.js" async defer></script>
+  </head>
+  <body>
+    <div id="container">
+      {{> (lookup this "page_type") }}
+      <footer>
+      </footer>
+    </div>
+  </body>
+</html>
+"#;
+
+const INDEX_TEMPLATE: &str = r#"
+<section>
+ <h1>{{title}}</h1>
+</section
+"#;
+
+const PAGE_TEMPLATE: &str = r#"
+{{> nav}}
+<section>
+  {{{contents}}}
+</section>
+"#;
+
+const POSTS_TEMPLATE: &str = r#"
+{{> nav}}
+<section>
+  <ul id="posts-list">
+    {{#each posts}}
+    <li class="posts-list-item">
+      <div class="posts-list-item-title">{{this.title}}</div>
+      <div class="posts-list-item-time">{{this.created_at}}</div>
+    </li>
+    {{/each}}
+  </ul>
+</section>
+"#;
+
+const POST_TEMPLATE: &str = r#"
+{{> nav}}
+<section>
+  {{{contents}}}
+</section>
+"#;
+
+const NAV_TEMPLATE: &str = r#"
+<nav>
+  <a href="/">Home</a>
+  {{#each path}}
+    <span class="breadcrumb">></span>
+    <a href="{{link}}">{{name}}</a>
+  {{/each}}
+</nav>
+"#;
+
+#[derive(EnumIter, Display)]
+#[strum(serialize_all = "lowercase")]
+pub enum TemplateName {
+    Base,
+    Index,
+    Page,
+    Posts,
+    Post,
+    Nav,
+}
+
+impl TemplateName {
+    pub fn template_str(&self) -> &str {
+        match self {
+            TemplateName::Base => BASE_TEMPLATE,
+            TemplateName::Index => INDEX_TEMPLATE,
+            TemplateName::Page => PAGE_TEMPLATE,
+            TemplateName::Posts => POSTS_TEMPLATE,
+            TemplateName::Post => POST_TEMPLATE,
+            TemplateName::Nav => NAV_TEMPLATE,
+        }
+    }
+}
